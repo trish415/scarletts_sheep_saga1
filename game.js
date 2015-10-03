5194b6495e49bb6d47176c7a0688372ec2f3ff66
Trish O'Connor <pjoconnor415@gmail.com>
2015-10-03 12:20:34 -0400

    Refactor setup code into setup.js
    Keep the game.js file clean by separating background setup

--- game.js
+++ game.js
@@ -8,24 +8,12 @@ function preload() {
     game.load.image('platform', 'art/platform.png');
 }
 
-var background;
-var platforms;
 var PLATFORM_HEIGHT = 32;
 
 function create(){
-    //set background
-    background = game.add.sprite(0,0,'background');
-    background.scale.set(0.5,0.5);
-    //set up platforms
-    platforms = game.add.group();
-    platforms.enableBody = true;
-    var bottom = platforms.create(0, game.world.height - PLATFORM_HEIGHT, 'platform');
-    var middle = platforms.create(0, game.world.height - 5*PLATFORM_HEIGHT, 'platform');
-    var top = platforms.create(0, game.world.height - 9*PLATFORM_HEIGHT, 'platform');
-    platforms.forEach(function(item){
-        item.scale.setTo(2,1);
-        item.body.immovable = true;
-    })
+    //setup
+    initializeBackground();
+    initializePlatforms();
 
 }
 
