c1e90fe2f70d8c9a890dbd4a858b77cdba9f8db5
Trish O'Connor <pjoconnor415@gmail.com>
2015-10-03 12:12:21 -0400

    Add platforms
    Three platforms added
    Intended use: scarlett and sheep are on platforms, player goes
    between platforms using up/down controls`

--- game.js
+++ game.js
@@ -5,14 +5,27 @@ var game = new Phaser.Game(boundsX, boundsY, Phaser.AUTO, "game", {preload:prelo
 function preload() {
     //load art
     game.load.image('background', 'art/background.png');
+    game.load.image('platform', 'art/platform.png');
 }
 
 var background;
+var platforms;
+var PLATFORM_HEIGHT = 32;
 
 function create(){
     //set background
     background = game.add.sprite(0,0,'background');
     background.scale.set(0.5,0.5);
+    //set up platforms
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    var bottom = platforms.create(0, game.world.height - PLATFORM_HEIGHT, 'platform');
+    var middle = platforms.create(0, game.world.height - 5*PLATFORM_HEIGHT, 'platform');
+    var top = platforms.create(0, game.world.height - 9*PLATFORM_HEIGHT, 'platform');
+    platforms.forEach(function(item){
+        item.scale.setTo(2,1);
+        item.body.immovable = true;
+    })
 
 }
 
